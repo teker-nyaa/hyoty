6468b7ae162373601b93cbda8dc1ddddad741d06
Teker <ellieputnam5@gmail.com>
2025-03-13 14:50:35 +0000

    working on it </3

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const quotes=["I love ninjas. I don't know any of them personally, but they're really cool", "BOO!", "Ik hatja rumonins", "fwæh", "if John Carmack uses bottom case in every file, is he a bottom?", "y'all ruined vercel", "ea-nasir is my idol", "Peguin walks with me!", "Kayne"];
+var quote = quotes[Math.floor(Math.random() * quotes.length)]
+
+$("#quote").text = quotes;
